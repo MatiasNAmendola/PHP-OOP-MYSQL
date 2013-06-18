a5f09ec79df831b966bc1065e33e578a4f226496
Niall Kelly <niall_kelly@manx.net>
2013-06-18 11:33:54 +0100

    Created basic db.class.php

---
+++ includes/db.class.php
@@ -0,0 +1,29 @@
+<?php
+
+	class Database {
+
+		// Connect to the database.
+		public function connect() { }
+
+		// Disconnect from the database.
+		public function disconnect() {}
+
+		// SELECT query
+		public function select() {}
+
+		// INSERT query
+		public function insert() {}
+
+		// DELETE QUERY
+		public function delete() {}
+
+		// UPDATE QUERY
+		public function update() {}
+
+		// Any SQL Query
+		public function query() {}
+
+
+	}
+
+?>
